8f9a81cb96c04575c2d88e6a0b907408af56e2d5
unknown <chanvicheka.ouk@gmail.com>
2025-09-26 01:06:58 -0400

    Greatest Commong Divisor of string v2 (after understand the vague problem statement) with my implementation beat 100% of the rest with 0ms

---
+++ greatest_common_divisor_of_strings2/greatest_common_divisor_of_strings2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greatest_common_divisor_of_strings2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GcdOfStrings("ABCABC", "ABC");
+            Debug.Assert(result.Equals("ABC"));
+
+            result = GcdOfStrings("ABABAB", "ABAB");
+            Debug.Assert(result.Equals("AB"));
+
+            result = GcdOfStrings("LEET", "CODE");
+            Debug.Assert(result.Equals(""));
+
+            result = GcdOfStrings("ABCDEF", "ABC");
+            Debug.Assert(result.Equals(""));
+        }
+
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            if (str1 + str2 != str2 + str1)
+                return "";
+
+            int lengthStr1 = str1.Length;
+            int lengthStr2 = str2.Length;
+
+            int gcd = 0;
+            while (lengthStr2 != 0)
+            {
+                int temp = lengthStr2;
+                lengthStr2 = lengthStr1 % lengthStr2;
+                lengthStr1 = temp;
+            }
+
+            gcd = lengthStr1;
+
+            string stringGCD = str1.Substring(0, gcd);
+            var result1 = str1.Replace(stringGCD, "");
+            var result2 = str2.Replace(stringGCD, "");
+
+            if (string.IsNullOrEmpty(result1) && string.IsNullOrEmpty(result2))
+            {
+                return stringGCD;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ greatest_common_divisor_of_strings2/greatest_common_divisor_of_strings2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("greatest_common_divisor_of_strings2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("greatest_common_divisor_of_strings2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdd73507-2263-4c9f-a87f-02b12d95205b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
