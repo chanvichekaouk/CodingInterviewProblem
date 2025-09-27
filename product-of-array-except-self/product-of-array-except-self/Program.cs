79a51075e82dcc7dd5da26a64c5f59a7316d5079
unknown <chanvicheka.ouk@gmail.com>
2025-09-27 16:18:22 -0400

    Product of Array except self with my implementation in C#. I think this should be a hard problem instead of meduim. the solution is finding the multiplication of left items (prefix) at current index and store it at current index from left to right in the aux array of nums length then again travel from right to left, find the multipication of right items (postfix) at current index and multiply it with the aux array at current index

---
+++ product-of-array-except-self/product-of-array-except-self/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_of_array_except_self
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ProductExceptSelf(new int[] { -1, 1, 0, -3, 3 });
+            Debug.Assert(result.SequenceEqual(new int[] { 0, 0, 9, 0, 0 }));
+
+            result = ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+            Debug.Assert(result.SequenceEqual(new int[] { 24, 12, 8, 6 }));
+        }
+
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+
+            int[] answers = new int[nums.Length];
+
+            // left to right with left or suffix multiply as result of each walking index
+            int suffixMultiply = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                answers[i] = suffixMultiply;
+                suffixMultiply *= nums[i];
+            }
+
+            // right to left with right side multiply or posfix multiply as result of each walking index
+            int postfixMultiply = 1;            
+            for (int i = nums.Length -1; i >=0 ; i--)
+            {
+                answers[i] *= postfixMultiply;
+                postfixMultiply *= nums[i];
+            }
+
+            return answers;
+
+        }
+    }
+}
