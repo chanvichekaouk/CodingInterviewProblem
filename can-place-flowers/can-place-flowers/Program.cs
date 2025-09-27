cad062d98d599b23b6286ebc1dfa3857c022b424
unknown <chanvicheka.ouk@gmail.com>
2025-09-26 22:16:59 -0400

    can place flowers with my implementation in c# beat runtime by 100% to the rest: run in 1ms

---
+++ can-place-flowers/can-place-flowers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace can_place_flowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result = CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1);
+
+            //var result = CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 2);
+
+            var result = CanPlaceFlowers(new int[] { 0, 0, 1, 0, 0 }, 1);
+        }
+
+        public static bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int plantedCount = 0;
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 1)
+                    continue;
+
+                bool isAdjacent = false;
+                if (i - 1 >= 0)
+                {
+                    if (flowerbed[i - 1] == 1)
+                        isAdjacent = true;
+                }
+
+                if(i + 1 < flowerbed.Length)
+                {
+                    if (flowerbed[i + 1] == 1)
+                        isAdjacent = true;
+                }
+
+                if (!isAdjacent)
+                {
+                    flowerbed[i] = 1;
+                    plantedCount++;
+                }
+            }
+            return plantedCount >= n;
+        }
+    }
+}
---
+++ can-place-flowers/can-place-flowers/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("can-place-flowers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("can-place-flowers")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1743095-fbc7-4970-aa3f-d8e21364baff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
