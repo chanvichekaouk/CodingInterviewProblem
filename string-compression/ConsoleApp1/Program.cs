d5e730be1846a4ff28f099d9eafcbdac22cc85db
unknown <chanvicheka.ouk@gmail.com>
2025-09-30 22:53:55 -0400

    string compression with c# implementation

---
+++ string-compression/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Compress(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' });
+            result = Compress(new char[] { 'a'});
+            result = Compress(new char[] { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' });
+        }
+
+        public static int Compress(char[] chars)
+        {
+
+            
+
+            Dictionary<char, int> myCharCount = new Dictionary<char, int>();
+            int count = 1;
+            char myChar = chars[0];
+            string myString = "" + chars[0];
+            for(int i=0; i< chars.Length; i++)
+            {
+
+                if (chars[i] == myChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    myString += "" + count;
+                }
+
+                //if (myCharCount.ContainsKey(c))
+                //    myCharCount[c]++;
+                //else
+                //    myCharCount[c] = 1;
+            }
+
+            //string myString = "";
+            //foreach (KeyValuePair<char,int> kp in myCharCount)
+            //{
+            //    if (kp.Value == 1)
+            //    {
+            //        myString += kp.Key + "";
+            //    }
+            //    else
+            //    {
+            //        myString += kp.Key + "" + kp.Value;
+            //    }                    
+            //}
+
+            //myString.CopyTo(0, chars, 0, myString.Length);
+            //int count = myString.Length;
+            return count;
+        }
+    }
+}
